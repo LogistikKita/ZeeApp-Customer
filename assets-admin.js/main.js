5b32a58b195f07ae200d62f18974c20349235f87
LogistikKita <logistikkita.assist@gmail.com>
2025-06-21 15:25:18 +0700

    Delete assets-admin.js directory

--- assets-admin.js/main.js
+++
@@ -1,4 +0,0 @@
-// File: /assets-admin/js/main.js document.getElementById('toggleBtn').addEventListener('click', () => { const sidebar = document.getElementById('sidebar'); const logoText = document.getElementById('logo-text'); const labels = document.querySelectorAll('.menu-item .label');
-
-if (sidebar.classList.contains('md:w-64')) { sidebar.classList.remove('md:w-64'); sidebar.classList.add('md:w-20'); logoText.classList.add('hidden'); labels.forEach(label => label.classList.add('hidden')); } else { sidebar.classList.remove('md:w-20'); sidebar.classList.add('md:w-64'); logoText.classList.remove('hidden'); labels.forEach(label => label.classList.remove('hidden')); } });
-
