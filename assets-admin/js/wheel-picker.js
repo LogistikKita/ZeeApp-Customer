8c42a7f245b5376466d7b92985b0606e03d40370
LogistikKita <logistikkita.assist@gmail.com>
2025-06-21 16:05:51 +0700

    Create wheel-picker.js

---
+++ assets-admin/js/wheel-picker.js
@@ -0,0 +1,5 @@
+// File: wheel-picker.js
+document.addEventListener("DOMContentLoaded", () => {
+  const filterBtn = document.getElementById("filterBtn");
+  const pickerModal = document.getElementById("pickerModal");
+  const btnApply = document.getElementById("
