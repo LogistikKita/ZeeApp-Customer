c7f8d1107aaa2bd4e0da4263fe07b945bb17430e
LogistikKita <logistikkita.assist@gmail.com>
2025-11-08 15:16:17 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js — Supabase Settings
-const supabaseUrl = 'https://mrghlcedtafomwnznywf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZ2hsY2VkdGFmb213bnpueXdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMTAwMTcsImV4cCI6MjA2NTg4NjAxN30.vCLC9w8sT0-4uMLFt9c-3BsFeLVco68ajtsu2ZQUiWs';
-const tableName = 'Data_Barang';
